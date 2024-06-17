5de1dba2d6c98d3b2ec695d3284d45f961aeb0d9
nanashi-1 <bagaresalken@gmail.com>
2024-06-17 14:43:48 +0800

    delete quick install script

--- quick-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/nanashi-1/osu-lazer-manager/releases/latest/download/osu-lazer-manager.tar.xz
-tar -xvf osu-lazer-manager.tar.xz
-mkdir -p ~/.local/bin/
-mv osu-lazer-manager ~/.local/bin/osu-lazer-manager
-rm osu-lazer-manager.tar.xz
