84062bc006fdab16ca4cc33bab52dd56b931fdd1
nanashi-1 <bagaresalken@gmail.com>
2024-06-20 19:22:24 +0800

    add better error messages

--- src/run.rs
+++ src/run.rs
@@ -1,3 +1,4 @@
+use anyhow::anyhow;
 use osu_lazer_manager::{
     config::get_default_version, constants::REPOSITORY, paths::get_directory_path,
 };
@@ -5,13 +6,21 @@ use std::{os::unix::process::CommandExt, process::Command};
 
 pub fn run(version: &str) -> anyhow::Result<()> {
     let version = match version {
-        "default" => get_default_version()?,
+        "default" => {
+            get_default_version().map_err(|e| anyhow!("Failed to get default version: {}", e))?
+        }
         _ => version.to_owned(),
     };
 
     println!("Running osu!lazer {}", version);
 
-    Command::new(get_directory_path()?.join(REPOSITORY).join(version)).exec();
+    Command::new(
+        get_directory_path()
+            .map_err(|e| anyhow!("Failed to get osu-lazer-manager data directory: {}", e))?
+            .join(REPOSITORY)
+            .join(version),
+    )
+    .exec();
 
     Ok(())
 }
